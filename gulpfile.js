6417cadd12ae3586195ac42593854d6bc6a490f4
John316 <John316@i.ua>
2016-11-14 11:31:46 +0200

    Change question store

--- gulpfile.js
+++ gulpfile.js
@@ -12,19 +12,20 @@ require('laravel-elixir-vue-2');
  | file for our application, as well as publishing vendor resources.
  |
  */
-elixir(mix => {
+elixir(function (mix) {
     mix.browserify([
         'main.js'
-    ], 'public/js')
-    .sass('app.scss')
+    ], 'public/js');
+
+    mix.sass('app.scss')
     .styles([
         'app.css'
-    ], 'public/css')
-    .scripts([
+    ], 'public/css');
+
+    mix.scripts([
         '../old-js/browser.min.js',
         '../old-js/pusher.min.js',
         '../old-js/highcharts.js',
-        //'../old-js/chart-themes.js',
         '../old-js/charts.js',
         '../old-js/lang.js',
         '../old-js/script.js'
